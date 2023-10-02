c114ebf1036141a95899c32bd17b870dd1558ba8
俞荃友 <alfredoyu@alfs-mbp-16inch.home>
2023-10-01 22:41:29 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,20 @@
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial(); 
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2); 
-		Polynomial s = p1.add(p2); 
-		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s"); 
-		else
-			System.out.println("1 is not a root of s");
-
+		Polynomial p = new Polynomial(new double[] {1, 2, 3}, new int[] {2, 1, 0});
+        
+        double val = p.evaluate(2); 
+        System.out.println("p(2) = " + val);
+        
+        boolean rootCheck = p.hasRoot(0);
+        System.out.println("Is 0 a root for System.out.print();? " + rootCheck);
+        
+        Polynomial p2 = new Polynomial(new double[] {1, 2}, new int[] {1, 0});
+        Polynomial sum = p.add(p2);
+        double val1 = sum.evaluate(2); 
+        System.out.println("p + p2: " + val1);
+        
+        Polynomial m = p.multiply(p2);
+        double val2 = m.evaluate(2); 
+        System.out.println("p * p2: " + val2);
 	}
 }
